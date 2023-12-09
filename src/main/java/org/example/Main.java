609b219eaad7b11ea790a238b99d57659a8be8f2
ziaro <zia.ro@live.com>
2023-12-09 11:34:34 -0500

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] arg){
+        double distance, mass;
+        float change;
+        distance = 1.495979E11;
+        mass = 4.989E30;
+        change = 1.25f;
+
+        System.out.println(distance);
+        System.out.println(mass);
+        System.out.println(change);
+    }
+}
